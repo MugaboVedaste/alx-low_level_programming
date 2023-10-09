0fe94b41362ca108da9fe4f6c6056c09e455ac88
MugaboVedaste <mugabovedaste30@gmail.com>
2023-10-09 08:59:35 -0700

    commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,27 +9,27 @@
  */
 int main(void)
 {
-    int num1, num2;
+	int num1, num2;
 
-    for (num1 = 0; num1 <= 99; num1++)
-    {
-        for (num2 = num1 + 1; num2 <= 99; num2++)
-        {
-            putchar((num1 / 10) + '0');   /* Print the tens digit of num1 */
-            putchar((num1 % 10) + '0');   /* Print the ones digit of num1 */
-            putchar(' ');                /* Print a space */
-            putchar((num2 / 10) + '0');   /* Print the tens digit of num2 */
-            putchar((num2 % 10) + '0');   /* Print the ones digit of num2 */
+	for (num1 = 0; num1 <= 99; num1++)
+	{
+		for (num2 = num1 + 1; num2 <= 99; num2++)
+		{
+			putchar((num1 / 10) + '0');   /* Print the tens digit of num1 */
+			putchar((num1 % 10) + '0');   /* Print the ones digit of num1 */
+			putchar(' ');                /* Print a space */
+			putchar((num2 / 10) + '0');   /* Print the tens digit of num2 */
+			putchar((num2 % 10) + '0');   /* Print the ones digit of num2 */
 
-            if (!(num1 == 98 && num2 == 99))
-            {
-                putchar(',');            /* Print a comma */
-                putchar(' ');            /* Print a space */
-            }
-        }
-    }
+			if (!(num1 == 98 && num2 == 99))
+			{
+				putchar(',');            /* Print a comma */
+				putchar(' ');            /* Print a space */
+			}
+		}
+	}
 
-    putchar('\n');  /* Print a newline */
+	putchar('\n');  /* Print a newline */
 
-    return (0);
+	return (0);
 }
