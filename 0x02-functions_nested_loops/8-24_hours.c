519bdc9fbb24c7b8184f94453023188117c296b0
MugaboVedaste <mugabovedaste30@gmail.com>
2023-10-10 03:46:40 -0700

    commit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every minute of the day of Jack Bauer.
+ */
+void jack_bauer(void)
+{
+    int hour, minute;
+
+    for (hour = 0; hour < 24; hour++)
+    {
+        for (minute = 0; minute < 60; minute++)
+        {
+            /* Print hour (two digits) */
+            _putchar('0' + (hour / 10));
+            _putchar('0' + (hour % 10));
+
+            /* Separator */
+            _putchar(':');
+
+            /* Print minute (two digits) */
+            _putchar('0' + (minute / 10));
+            _putchar('0' + (minute % 10));
+
+            /* Newline */
+            _putchar('\n');
+        }
+    }
+}
