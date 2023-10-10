8a90f2a5b2852ea06d18b9c20595ef49c2f13893
MugaboVedaste <mugabovedaste30@gmail.com>
2023-10-10 04:51:10 -0700

    commit

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int count, n;
+    unsigned long int fib1 = 1, fib2 = 2, next;
+
+    count = 2;  /* Start with 2 because we already have 1 and 2 */
+
+    printf("%lu, %lu", fib1, fib2); /* Print the first two Fibonacci numbers */
+
+    while (count < 98)
+    {
+        next = fib1 + fib2;
+        fib1 = fib2;
+        fib2 = next;
+
+        printf(", %lu", next);
+
+        count++;
+    }
+
+    printf("\n");
+
+    return (0);
+}
